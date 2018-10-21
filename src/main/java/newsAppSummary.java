dc9cf360b513823a6702f37eb513dfe14c0f0589
Din Stolak <dee@dins-mbp.home>
2018-10-21 23:31:32 +0100

    started naming and translating my attempt at the logic side in java.

---
+++ src/main/java/newsAppSummary.java
@@ -0,0 +1,17 @@
+//allows usage of json with java
+import org.json.*;
+
+//myConstants class acts as container
+//static mixed with fixed will create the
+//closest equivalent to a constant in Java
+public class myConstants {
+  static String api_key = 7;
+  static String article = document.getElementById('article');
+  static String articlesList = document.getElementById('articles');
+  static String articleContent = document.getElementById('article-content');
+  static String button = document.getElementById('go-back');
+   
+  
+}
+
+
